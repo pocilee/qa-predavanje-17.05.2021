d9e980c7e53b74fa83b00cb5716c2018f91b6cd9
pocilee <goran.pobric@gmail.com>
2021-05-17 20:31:35 +0200

    third

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -7,5 +7,27 @@ describe('login spec', () => {
         cy.get('a[href="/login"]').click()
         // cy.get('a[class="nav-link nav-buttons"]').eq(0).click()  -- drugi nacin
     })
+
+    it('enter email', () => {
+        cy.get('input[id="email"]').type('goran.pobric@gmail.com')
+    })
+
+    it('enter password', () => {
+        cy.get('input[id="password"]').type('pobra111')
+    })
+
+    it('submit', () => {
+        cy.get('button[class="btn btn-custom"]').click()
+    })
 })
+
+describe('logout', () => {
+    it('click logout', () => {
+        cy.get('.ml-auto > :nth-child(3) > .nav-link').click()
+        // cy.get('ul[class="navbar-nav ml-auto mt-2 mt-lg-0"] > li[class="nav-item"] > a[class="nav-link nav-buttons"]').click()
+    })
+})
+
+
+
 
