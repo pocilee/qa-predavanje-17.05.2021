352a2abc71a95b9981ce9d26f4c0c7c90d4157f5
pocilee <goran.pobric@gmail.com>
2021-05-17 21:15:04 +0200

    third

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,13 +1,21 @@
 describe('login spec', () => {
     it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
+        cy.visit('')
     })
 
     it('click login button', () => {
         cy.get('a[href="/login"]').click()
         // cy.get('a[class="nav-link nav-buttons"]').eq(0).click()  -- drugi nacin
     })
+})
 
+describe('login - negative', () => {
+    it('enter email - neg. without @ sign', () => {
+        cy.get('input[id="email"]').type('goran.pobricgmail.com')
+    })
+})
+
+describe('login spec', () => {
     it('enter email', () => {
         cy.get('input[id="email"]').type('goran.pobric@gmail.com')
     })
@@ -27,6 +35,11 @@ describe('logout', () => {
         // cy.get('ul[class="navbar-nav ml-auto mt-2 mt-lg-0"] > li[class="nav-item"] > a[class="nav-link nav-buttons"]').click()
     })
 })
+
+// it.only - odradice samo ovaj test
+// it.wait(4000) - saceka zadato vreme, pa onda radi test
+
+
 
 
 
---
+++ cypress/integration/negative.register.spec.js
@@ -0,0 +1,22 @@
+describe('register page', () => {
+    it('visit gallery app', () => {
+        cy.visit('')
+    })
+
+    it('click register button', () => {
+        cy.get('a[href="/register"]').click()
+    })
+})
+
+describe('register spec', () => {
+    it('fill registration forms', () => {
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simomatavulj1gmail.com')
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click()
+        
+    })
+})
