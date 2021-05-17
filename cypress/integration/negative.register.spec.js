d0956306479bdd4bf04214e7fa6cecec005e8643
pocilee <goran.pobric@gmail.com>
2021-05-17 21:31:42 +0200

    third

--- cypress/integration/negative.register.spec.js
+++ cypress/integration/negative.register.spec.js
@@ -12,11 +12,28 @@ describe('neg. register spec', () => {
     it('fill registration forms', () => {
         cy.get('input[id="first-name"]').type('Simo')
         cy.get('input[id="last-name"]').type('Matavulj')
-        cy.get('input[id="email"]').type('simomatavulj1gmail.com')
+        cy.get('input[id="email"]').type('simomatavulj1gmail.com') // bez @
         cy.get('input[id="password"]').type('12345678')
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
-        cy.get('button[type="submit"]').click()
-        
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simomatavulj1@gmail') // bez .com
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simomatavulj1@gmail.com')
+        cy.get('input[id="password"]').type('1')
+        cy.get('input[id="password-confirmation"]').type('1')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+
     })
 })
