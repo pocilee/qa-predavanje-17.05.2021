9d6cc67039c6d3aa5979c98ffeb2984ae0125aba
pocilee <goran.pobric@gmail.com>
2021-05-19 14:53:27 +0200

    domaci 19.05.2021

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,43 @@
+describe('login spec', () => {
+    it('visit gallery app', () => {
+        cy.visit('')
+    })
+
+    it('click login button', () => {
+        cy.get('a[href="/login"]').click()
+        // cy.get('a[class="nav-link nav-buttons"]').eq(0).click()  -- drugi nacin
+    })
+})
+
+describe('login - negative', () => {
+    it('enter email - neg. without @ sign', () => {
+        cy.get('input[id="email"]').type('goran.pobricgmail.com')
+    })
+})
+
+describe('login spec', () => {
+    it('enter email', () => {
+        cy.get('input[id="email"]').type('goran.pobric@gmail.com')
+    })
+
+    it('enter password', () => {
+        cy.get('input[id="password"]').type('pobra111')
+    })
+
+    it('submit', () => {
+        cy.get('button[class="btn btn-custom"]').click()
+    })
+})
+
+describe('logout', () => {
+    it('click logout', () => {
+        cy.get('.ml-auto > :nth-child(3) > .nav-link').click()
+        // cy.get('ul[class="navbar-nav ml-auto mt-2 mt-lg-0"] > li[class="nav-item"] > a[class="nav-link nav-buttons"]').click()
+    })
+})
+
+// it.only - odradice samo ovaj test
+// it.wait(4000) - saceka zadato vreme, pa onda radi test
+
+
+
---
+++ cypress/integration/negative.register.spec.js
@@ -0,0 +1,64 @@
+describe('register page', () => {
+    it('visit gallery app', () => {
+        cy.visit('https://gallery-app.vivifyideas.com/')
+    })
+
+    it('click register button', () => {
+        cy.get('a[href="/register"]').click()
+    })
+})
+
+describe('neg. register spec', () => {
+    it('fill registration forms', () => {
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simomatavulj11gmail.com') // without @
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('@gmail.com') // without body before @
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simomatavulj11@gmail') // without .com
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simo@matavulj11@gmail') // 2 @ signs
+        cy.get('input[id="password"]').type('12345678')
+        cy.get('input[id="password-confirmation"]').type('12345678')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simomatavulj11@gmail.com')
+        cy.get('input[id="password"]').type('1234578')
+        cy.get('input[id="password-confirmation"]').type('12345') //password confirmation wrong
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+        cy.get('input[id="first-name"]').type('Simo')
+        cy.get('input[id="last-name"]').type('Matavulj')
+        cy.get('input[id="email"]').type('simomatavulj11@gmail.com')
+        cy.get('input[id="password"]').type('123457') // password.length = 7
+        cy.get('input[id="password-confirmation"]').type('1234567')
+        cy.get('input[class="form-check-input"]').check()
+        cy.get('button[type="submit"]').click().wait(3000)
+        cy.reload()
+
+    })
+})
+
