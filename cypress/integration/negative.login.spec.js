76925e80847062f03796d2a324bb8b63ebf4d087
pocilee <goran.pobric@gmail.com>
2021-05-21 18:30:21 +0200

    predavanje 21.05.2021

--- cypress/integration/negative.login.spec.js
+++ cypress/integration/negative.login.spec.js
@@ -1,54 +1,62 @@
 describe('login spec', () => {
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
+
     
-    it('click login button', () => {
-        cy.get('a[href="/login"]').click()
-    })
+    // it('visit gallery app', () => {
+    //     cy.visit('https://gallery-app.vivifyideas.com/')
+    // })
+    
+    // it('click login button', () => {
+    //     cy.get('a[href="/login"]').click()
+    // })
 })
 
 describe('login - negative', () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+        cy.get('a[href="/login"]').click()
+      })
+
     it('email no @ sign', () => {
         cy.get('input[id="email"]').type('goran.pobricgmail.com')
         cy.get('input[id="password"]').type('pobra111')
         cy.get('button[class="btn btn-custom"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
 
     it('email no .com', () => {
         cy.get('input[id="email"]').type('goran.pobric@gmail')
         cy.get('input[id="password"]').type('pobra111')
         cy.get('button[class="btn btn-custom"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
 
     it('wrong email', () => {
         cy.get('input[id="email"]').type('goranpobric@gmail.com')
         cy.get('input[id="password"]').type('pobra111')
         cy.get('button[class="btn btn-custom"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
 
     it('wrong password', () => {
         cy.get('input[id="email"]').type('goran.pobric@gmail.com')
         cy.get('input[id="password"]').type('pobra11')
         cy.get('button[class="btn btn-custom"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
 
     it('no password', () => {
         cy.get('input[id="email"]').type('goran.pobric@gmail.com')
         // cy.get('input[id="password"]').type('pobra11')
         cy.get('button[class="btn btn-custom"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
 
     it('no email', () => {
         // cy.get('input[id="email"]').type('goran.pobric@gmail.com')
         cy.get('input[id="password"]').type('pobra11')
         cy.get('button[class="btn btn-custom"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
 
 
--- cypress/integration/negative.register.spec.js
+++ cypress/integration/negative.register.spec.js
@@ -1,14 +1,14 @@
 describe('register page', () => {
-    it('visit gallery app', () => {
-        cy.visit('https://gallery-app.vivifyideas.com/')
-    })
 
-    it('click register button', () => {
-        cy.get('a[href="/register"]').click()
-    })
+    
+   
 })
 
 describe('neg. register spec', () => {
+    beforeEach(() => {
+        cy.visit('/')
+        cy.get('a[href="/register"]').click()
+      })
     it('email no @ sign', () => {
         cy.get('input[id="first-name"]').type('Simo')
         cy.get('input[id="last-name"]').type('Matavulj')
@@ -17,7 +17,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('email without body before @', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -27,7 +27,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('email without .com', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -37,7 +37,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('email with 2 @ signs', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -47,7 +47,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('password confirmation wrong', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -57,7 +57,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345') //password confirmation wrong
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('password.length = 7', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -67,7 +67,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('1234567')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('empty first name', () => {
         // cy.get('input[id="first-name"]').type('Simo') no 'first name'
@@ -77,7 +77,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('empty last name', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -87,7 +87,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('empty email', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -97,7 +97,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('empty password', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -107,7 +107,7 @@ describe('neg. register spec', () => {
         cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('empty confirmation password', () => {
         cy.get('input[id="first-name"]').type('Simo')
@@ -117,17 +117,17 @@ describe('neg. register spec', () => {
         // cy.get('input[id="password-confirmation"]').type('12345678')
         cy.get('input[class="form-check-input"]').check()
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
     it('no terms and conditions', () => {
         cy.get('input[id="first-name"]').type('Simo')
         cy.get('input[id="last-name"]').type('Matavulj')
         cy.get('input[id="email"]').type('simomatavulj123@gmail.com')
-        cy.get('input[id="password"]').type('1234578')
+        cy.get('input[id="password"]').type('12345678')
         cy.get('input[id="password-confirmation"]').type('12345678')
         // cy.get('input[class="form-check-input"]').check().
         cy.get('button[type="submit"]').click().wait(2000)
-        cy.reload()
+        // cy.reload()
     })
 })
 
